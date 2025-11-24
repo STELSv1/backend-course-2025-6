abfcae17add93c33976e4fc6e2efdd69ed13d622
Valentyn bc2025-6 <stelspro228@gmail.com>
2025-11-24 19:08:46 +0200

    Add HTML forms and GET /inventory endpoint

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+let inventory = [];
+
+app.get('/RegisterForm.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'RegisterForm.html'));
+});
+
+app.get('/SearchForm.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'SearchForm.html'));
+});
+
+app.get('/inventory', (req, res) => {
+    res.status(200).json(inventory);
+});
 
 program
   .requiredOption('-h, --host <host>', 'Server address')
