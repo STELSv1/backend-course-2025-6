f34e126455bf2de121387c4da2e28b4a1eb68883
Valentyn bc2025-6 <stelspro228@gmail.com>
2025-11-24 20:03:31 +0200

    Final: Add Swagger docs, error handling 405, and Postman collection

--- index.js
+++ index.js
@@ -4,29 +4,63 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+program
+  .requiredOption('-h, --host <host>', 'Server address')
+  .requiredOption('-p, --port <port>', 'Server port')
+  .requiredOption('-c, --cache <path>', 'Cache directory path');
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (!fs.existsSync(options.cache)) {
+  fs.mkdirSync(options.cache, { recursive: true });
+}
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, options.cache)
+        cb(null, options.cache);
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+        cb(null, Date.now() + '-' + file.originalname);
     }
 });
 const upload = multer({ storage: storage });
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+let inventory = [];
+
+app.get('/RegisterForm.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'RegisterForm.html'));
+});
+
+app.get('/SearchForm.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'SearchForm.html'));
+});
+
+app.get('/inventory', (req, res) => {
+    res.status(200).json(inventory);
+});
 
 app.post('/register', upload.single('photo'), (req, res) => {
     const { inventory_name, description } = req.body;
 
     if (!inventory_name) {
-        return res.status(400).send('Inventory name is required'); 
+        return res.status(400).send('Inventory name is required');
     }
 
     const newItem = {
-        id: Date.now().toString(), 
-        name: inventory_name,      
-        description: description || '', 
-        photo: req.file ? req.file.filename : null 
+        id: Date.now().toString(),
+        name: inventory_name,
+        description: description || '',
+        photo: req.file ? req.file.filename : null
     };
 
     inventory.push(newItem);
@@ -38,11 +72,10 @@ app.post('/search', (req, res) => {
     const item = inventory.find(i => i.id === id);
 
     if (!item) {
-        return res.status(404).send('Not Found'); 
+        return res.status(404).send('Not Found');
     }
 
     let responseItem = { ...item };
-
 
     if (has_photo === 'on' && responseItem.photo) {
         responseItem.description += ` (Photo link: /inventory/${item.id}/photo)`;
@@ -63,10 +96,9 @@ app.get('/inventory/:id/photo', (req, res) => {
     const item = inventory.find(i => i.id === req.params.id);
     if (!item || !item.photo) return res.status(404).send('Not Found');
 
-    res.setHeader('Content-Type', 'image/jpeg'); // [cite: 82]
+    res.setHeader('Content-Type', 'image/jpeg');
     res.sendFile(path.join(path.resolve(options.cache), item.photo));
 });
-
 
 app.put('/inventory/:id', (req, res) => {
     const item = inventory.find(i => i.id === req.params.id);
@@ -77,7 +109,6 @@ app.put('/inventory/:id', (req, res) => {
 
     res.json(item);
 });
-
 
 app.put('/inventory/:id/photo', upload.single('photo'), (req, res) => {
     const item = inventory.find(i => i.id === req.params.id);
@@ -98,36 +129,12 @@ app.delete('/inventory/:id', (req, res) => {
     inventory.splice(index, 1);
     res.status(200).send('Deleted');
 });
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
 
-let inventory = [];
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/RegisterForm.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'RegisterForm.html'));
+app.use((req, res) => {
+    res.status(405).send('Method not allowed');
 });
-
-app.get('/SearchForm.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'SearchForm.html'));
-});
-
-app.get('/inventory', (req, res) => {
-    res.status(200).json(inventory);
-});
-
-program
-  .requiredOption('-h, --host <host>', 'Server address')
-  .requiredOption('-p, --port <port>', 'Server port')
-  .requiredOption('-c, --cache <path>', 'Cache directory path');
-
-program.parse(process.argv);
-const options = program.opts();
-
-if (!fs.existsSync(options.cache)) {
-  fs.mkdirSync(options.cache, { recursive: true });
-  console.log(`Created cache directory: ${options.cache}`);
-}
 
 const server = http.createServer(app);
 server.listen(options.port, options.host, () => {
